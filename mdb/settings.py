8094b157ae7b58a92520815f0347b7523f867ce5
Simone <simoneungaro@hotmail.it>
2020-05-04 12:49:51 +0200

    Configuration settings.py for using MariaDB databases
    Imposto l'uso del database MariaDB al posto del database sqlite

--- mdb/settings.py
+++ mdb/settings.py
@@ -75,13 +75,21 @@ WSGI_APPLICATION = 'mdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+        'NAME': 'mdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
