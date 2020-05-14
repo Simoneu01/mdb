f3cbeac680f793ad93c5e47ea3b3159606f48325
Simone <simoneungaro@hotmail.it>
2020-05-14 11:21:09 +0200

    add frontend components

---
+++ frontend/src/components/Body.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Tiles/>
+        <Tiles/>
+    </div>
+</template>
+
+<script>
+    import Tiles from "./Tiles"
+    export default {
+        name: "Body",
+        components:{
+            Tiles
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <Body/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+    import Body from "./Body"
+    import Footer from "./Footer"
+
+    export default {
+        name: "Container",
+        components: {
+            Footer,
+            Body
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
