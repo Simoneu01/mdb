c42a7232524d3e8e6ea6a834d230e050a7b44db5
Simone <simoneungaro@hotmail.it>
2020-05-18 15:19:48 +0200

    Update Model

--- backend/mdb_app/models.py
+++ backend/mdb_app/models.py
@@ -7,78 +7,113 @@ class Article(models.Model):
     article_id = models.AutoField(primary_key=True)
     article_heading = models.CharField(max_length=250)
     article_body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Artista(models.Model):
     nome = models.CharField(max_length=50)
     cognome = models.CharField(max_length=50)
-    foto = models.ImageField(upload_to='uploads/%Y/%m/canzoni/artisti/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/canzoni/artisti/', default='uploads/default.png')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s %s' % (self.nome, self.cognome)
+
+    class Meta:
+        verbose_name_plural = "Artisti"
 
 
 class Album(models.Model):
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
     nome = models.CharField(max_length=50)
     pubblicazione = models.DateField()
-    copertina = models.ImageField(upload_to='uploads/%Y/%m/canzoni/album/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/canzoni/album/', default='uploads/default.png')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return 'Titolo Album: %s - Data Pubblicazione: %s' % (self.nome, self.pubblicazione)
+
 
 class Canzone(models.Model):
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     titolo = models.CharField(max_length=50)
-    copertina = models.ImageField(upload_to='uploads/%Y/%m/canzoni', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/canzoni', default='uploads/default.png')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s:%s' % (self.titolo, self.album)
 
+    class Meta:
+            verbose_name_plural = "Canzoni"
+
 class Genere(models.Model):
     nome = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '%s' % (self.nome)
+        return '%s' % self.nome
 
+    class Meta:
+        verbose_name_plural = "Generi"
 
 class Film(models.Model):
     titolo = models.CharField(max_length=50)
     pubblicazione = models.DateField()
     generi = models.ManyToManyField(Genere)
-    copertina = models.ImageField(upload_to='uploads/%Y/%m/film/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/film/', default='uploads/default.png')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '%s' % (self.titolo)
+        return '%s' % self.titolo
 
 
 class Attore(models.Model):
     nome = models.CharField(max_length=50)
     cognome = models.CharField(max_length=50)
     dob = models.DateField()
-    foto = models.ImageField(upload_to='uploads/%Y/%m/film/attori/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/film/attori/', default='uploads/default.png')
     films = models.ManyToManyField(Film)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s %s' % (self.nome, self.cognome)
 
+    class Meta:
+            verbose_name_plural = "Attori"
 
 class Scrittore(models.Model):
     nome = models.CharField(max_length=50)
     cognome = models.CharField(max_length=50)
     dob = models.DateField()
-    foto = models.ImageField(upload_to='uploads/%Y/%m/libri/scrittori/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/libri/scrittori/', default='uploads/default.png')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s %s' % (self.nome, self.cognome)
+
+    class Meta:
+            verbose_name_plural = "Scrittori"
 
 class Libro(models.Model):
     titolo = models.CharField(max_length=50)
     pubblicazione = models.DateField()
     generi = models.ManyToManyField(Genere)
-    copertina = models.ImageField(upload_to='uploads/%Y/%m/libri/', blank=True, null=True)
+    src = models.ImageField(upload_to='uploads/%Y/%m/libri/', default='uploads/default.png')
     scrittore = models.ForeignKey(Scrittore, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return 'Titolo: %s - Scrittore: %s' % (self.titolo, self.scrittore)
+
+    class Meta:
+        verbose_name_plural = "Libri"
