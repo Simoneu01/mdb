b1d98a944197540340cd62cf8f23964691590523
Simone <simoneungaro@hotmail.it>
2020-05-19 12:40:50 +0200

    Fix route & Add 404 view

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Film from './views/Film.vue'
-import Libri from "./views/Libri";
-import Musica from "./views/Musica";
+import Film from './views/Film/index.vue'
+import Libri from "./views/Libri/index";
+import Musica from "./views/Musica/index";
+import Container from "./components/Container"
+import {APIService} from './APIService';
+const apiService = new APIService();
 
 Vue.use(Router)
 
@@ -11,24 +14,60 @@ export default new Router({
     routes: [
         {
             path: '/',
-            alias: '/home',
-            name: 'Home',
-            component: Home
+            component: Container,
+            children: [
+                {
+                    path: '',
+                    component: Home,
+                    alias: 'home'
+                },
+                // Film will be rendered inside Container's <router-view>
+                // when /film is matched
+                {
+                    path: 'film',
+                    component: Film
+                },
+                // Libri will be rendered inside Container's <router-view>
+                // when /film is matched
+                {
+                    path: 'libri',
+                    component: Libri
+                },
+                // Musica will be rendered inside Container's <router-view>
+                // when /film is matched
+                {
+                    path: 'musica',
+                    component: Musica
+                },
+                {
+                    path: '/film/:id',
+                    component: () => import('./views/Film/view'),
+                    beforeEnter: (to, from, next) => {
+
+                        function isValid (id) {
+                            return apiService.getFilm(id, (err) => {
+                                if (err) {
+                                    next({ name: 'not-found' });
+                                } else {
+                                    next();
+                                }
+                            })
+                        }
+
+                        isValid(to.params.id);
+                    }
+                }
+            ]
         },
         {
-            path: '/film',
-            name: 'Film',
-            component: Film
+            path: '404',
+            component: () => import('./views/404'),
+            name: 'not-found'
         },
         {
-            path: '/musica',
-            name: 'Musica',
-            component: Musica
-        },
-        {
-            path: '/libri',
-            name: 'Libri',
-            component: Libri
+            path: '*',
+            component: () => import('./views/404'),
+            name: 'not-found'
         }
     ]
 })
---
+++ frontend/src/views/404.vue
@@ -0,0 +1,53 @@
+<template>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title">
+                   404 Be right back.
+                </div>
+                <div>
+                    <router-link to="/">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Torna alla Home</button>
+                    </router-link>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+     html, body {
+         background-color: #fff;
+         color: #636b6f;
+         font-family: 'Raleway', sans-serif;
+         font-weight: 100;
+         height: 100vh;
+         margin: 0;
+     }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+</style>
